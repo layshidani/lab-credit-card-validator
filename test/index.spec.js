8191e45e8fb20ba28a10f626ac72754ee9160adc
hlays <lays.hidani@gmail.com>
2019-04-27 17:29:30 -0300

    add tests closes #8 closes #9

---
+++ test/index.spec.js
@@ -0,0 +1,28 @@
+const { assert } = require('chai');
+const cardValidator = require('../lib/index');
+
+describe('Validador de cartão de credito', () => {
+  describe('Deve retornar true para cartoes validos:', () => {
+    it('Mastercard - Deve retornar true: 5374661040114189', () => {
+      assert.equal(cardValidator.cardValidator('5374661040114189'), true);
+    });
+    it('Visa - Deve retornar true: 4556398657023626', () => {
+      assert.equal(cardValidator.cardValidator('4556398657023626'), true);
+    });
+    it('Elo - Deve retornar true: 6363688326577129', () => {
+      assert.equal(cardValidator.cardValidator('6363688326577129'), true);
+    });
+  });
+
+  describe('Deve retornar false para cartoes invalidos:', () => {
+    it('Vazio - Deve retornar false ', () => {
+      assert.equal(cardValidator.cardValidator(''), false);
+    });
+    it('Qtd de digitos inválida - Deve retornar false:  123456', () => {
+      assert.equal(cardValidator.cardValidator('123456'), false);
+    });
+    it('Numeros e letras - Deve retornar false: 1sd2d3456', () => {
+      assert.equal(cardValidator.cardValidator('1sd2d3456'), false);
+    });
+  });
+});
